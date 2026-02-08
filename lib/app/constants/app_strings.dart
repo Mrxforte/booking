e59c17e5726ebd6fe769a0be955d5f20416c0e87
MrxForte <urazimbetovazamat6@gmail.com>
2026-02-08 09:27:33 -0800

    added login screen

--- lib/app/constants/app_strings.dart
+++ lib/app/constants/app_strings.dart
@@ -1,3 +1,4 @@
 class AppStrings {
   static const String appName = 'Flutter Starter';
+  static const String welcomeMessage = 'Welcome to Booking app!';
 }
---
+++ lib/views/login/login_screen.dart
@@ -0,0 +1,61 @@
+import 'package:booking/app/constants/app_strings.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            // welcome text
+            Text(
+              AppStrings.welcomeMessage,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            // email and password fields without borders
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // login button
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: () {}, child: Text('Login')),
+            // sign up button
+            TextButton(
+              onPressed: () {},
+              child: Text('Don\'t have an account? Sign Up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
